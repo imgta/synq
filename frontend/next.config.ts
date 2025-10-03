1df2359c861ab60a02e7897211dcc7faa5c7baf2
Gordon Ta <gphamta+github@gmail.com>
2025-10-03 13:52:14 -0400

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,23 +1,7 @@
 import { type NextConfig } from 'next';
 import path from 'path';
 
-const EXTERNALS = [
-  'onnxruntime-node',
-  'sharp',
-];
-
 export default {
-  outputFileTracingIncludes: { '/api/**/*': ['./node_modules/**/*.wasm'] },
-  // serverExternalPackages: EXTERNALS,
-  // webpack(cfg) {
-  //   cfg.resolve.alias = {
-  //     ...(cfg.resolve.alias || {}),
-  //     'onnxruntime-node': false,
-  //     'sharp': false,
-  //   };
-  //   cfg.experiments = { ...(cfg.experiments || {}), asyncWebAssembly: true, layers: true };
-  //   return cfg;
-  // },
   experimental: {
     reactCompiler: true, // enable `babel-plugin-react-compiler@rc`
   },
