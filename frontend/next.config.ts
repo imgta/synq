107d664ee9eed3909c0da04a574364a169242e5b
Gordon Ta <gphamta+github@gmail.com>
2025-10-03 12:38:04 -0400

    hf/transformers and onnx external declaration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import { type NextConfig } from 'next';
 import path from 'path';
 
+const EXTERNALS = [
+  '@huggingface/transformers',
+  'onnxruntime-node',
+];
+
 export default {
+  outputFileTracingIncludes: { '/api/**/*': ['./node_modules/**/*.wasm'] },
+  serverExternalPackages: EXTERNALS,
+  webpack(cfg) {
+    cfg.experiments = { asyncWebAssembly: true, layers: true };
+    cfg.externals ||= [];
+    cfg.externals.push({ zipfile: 'commonjs zipfile' });
+    return cfg;
+  },
   experimental: {
     reactCompiler: true, // enable `babel-plugin-react-compiler@rc`
   },
